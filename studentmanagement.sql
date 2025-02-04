e7e688b0220958659468ad84df185d9e3ce8bf05
Andile Ndlovu <andilendlovu900@gmail.com>
2025-02-04 13:02:39 +0200

    Update studentmanagement.sql
    Fixed ORA-00001 error

--- studentmanagement.sql
+++ studentmanagement.sql
@@ -25,16 +25,16 @@ FOREIGN KEY(course_id) REFERENCES courses(course_id)
 );
 
 -- inserting student information
-INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(1, 'Andile', 'Ndlovu', TO_DATE('11-09-1990', 'DD-MM-YYYY', 'an@notreal.com');
-INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(2, 'Buhle', 'Mukhuba', TO_DATE('10-07-2003', 'DD-MM-YYYY', 'bm@notreal.com');
-INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(3, 'Khumbulani', 'Ndlovu', TO_DATE('04-10-2003', 'DD-MM-YYYY', 'kb@notreal.com');
+INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(1, 'Andile', 'Ndlovu', TO_DATE('11-09-1990', 'DD-MM-YYYY'), 'an@notreal.com');
+INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(2, 'Buhle', 'Mukhuba', TO_DATE('10-07-2003', 'DD-MM-YYYY'), 'bm@notreal.com');
+INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(3, 'Khumbulani', 'Ndlovu', TO_DATE('04-10-2003', 'DD-MM-YYYY'), 'kb@notreal.com');
 
 -- inserting course information
-INSERT INTO courses(course_id, course_name, course_description) VALUES(1, 'DALG103', 'Data Structures and Algorithms');
-INSERT INTO courses(course_id, course_name, course_description) VALUES(2, 'DBMS101', 'Introduction to databases and SQL');
-INSERT INTO courses(course_id, course_name, course_description) VALUES(3, 'NOPS101', 'Networks and Operating Systems');
+INSERT INTO courses(course_id, course_name, course_description) VALUES(11, 'DALG103', 'Data Structures and Algorithms');
+INSERT INTO courses(course_id, course_name, course_description) VALUES(12, 'DBMS101', 'Introduction to databases and SQL');
+INSERT INTO courses(course_id, course_name, course_description) VALUES(13, 'NOPS101', 'Networks and Operating Systems');
 
 -- enrolling students into courses
-INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(1, 1, 1, SYSDATE);
-INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(2, 2, 1, SYSDATE);
-INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(3, 3, 1, SYSDATE);
+INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(21, 1, 1, TRUNC(SYSDATE));
+INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(22, 2, 1, TRUNC(SYSDATE));
+INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(23, 3, 1, TRUNC(SYSDATE));
