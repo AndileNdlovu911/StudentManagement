71af16a3999e2d8aae8f226249311df689c08815
Andile Ndlovu <andilendlovu900@gmail.com>
2025-02-05 13:39:44 +0200

    Update studentmanagement.sql
    Another ORA0001 error fixed.

--- studentmanagement.sql
+++ studentmanagement.sql
@@ -30,11 +30,25 @@ INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(2,
 INSERT INTO students(student_id, f_name, l_name, date_of_birth, email) VALUES(3, 'Khumbulani', 'Ndlovu', TO_DATE('04-10-2003', 'DD-MM-YYYY'), 'kb@notreal.com');
 
 -- inserting course information
-INSERT INTO courses(course_id, course_name, course_description) VALUES(11, 'DALG103', 'Data Structures and Algorithms');
-INSERT INTO courses(course_id, course_name, course_description) VALUES(12, 'DBMS101', 'Introduction to databases and SQL');
-INSERT INTO courses(course_id, course_name, course_description) VALUES(13, 'NOPS101', 'Networks and Operating Systems');
+INSERT INTO courses(course_id, course_name, course_description) VALUES(1001, 'DALG103', 'Data Structures and Algorithms');
+INSERT INTO courses(course_id, course_name, course_description) VALUES(1002, 'DBMS101', 'Introduction to databases and SQL');
+INSERT INTO courses(course_id, course_name, course_description) VALUES(1003, 'NOPS101', 'Networks and Operating Systems');
 
 -- enrolling students into courses
-INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(21, 1, 1, TRUNC(SYSDATE));
-INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(22, 2, 1, TRUNC(SYSDATE));
-INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(23, 3, 1, TRUNC(SYSDATE));
+INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(21, 1, 1001, TRUNC(SYSDATE));
+INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(22, 2, 1001, TRUNC(SYSDATE));
+INSERT INTO enrollments(enrollment_id, student_id, course_id, enrollment_date) VALUES(23, 3, 1001, TRUNC(SYSDATE));
+
+-- Procedure for adding a student
+CREATE OR REPLACE PROCEDURE add_student(
+p_student_id IN NUMBER,
+p_f_name IN VARCHAR2,
+p_l_name IN VARCHAR2,
+p_date_of_birth IN DATE,
+p_email IN VARCHAR2
+)AS
+BEGIN
+INSERT INTO students (student_id, f_name, l_name, date_of_birth, email) VALUES (p_student_id, p_f_name, p_l_name, p_date_of_birth, p_email);
+COMMIT;
+DBMS_OUTPUT.PUT_LINE('Student added successfully!');
+END;
